67df9a069eebcb548abcaa492b4382a84a3e89f5
coolusernameswagohio <159517548+coolusernameswagohio@users.noreply.github.com>
2025-09-18 20:10:44 +0300

    added alot to the project

--- zoo/main/urls.py
+++ zoo/main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('contacts', views.show_contacts, name='contacts'),
     path('prices', views.show_prices, name='prices'),
     path('about', views.show_about, name='about'),
-    path('animals', views.show_animals, name='animals')
+    path('animals_u', views.show_animals, name='animals_u')
 ]
--- zoo/moderator/urls.py
+++ zoo/moderator/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_admin_main_page, name="moderator/index.html"),
-    path('create', views.animal_create, name="create")
+    path('', views.show_moderator_main_page, name="moderator/index.html"),
+    path('animals/create', views.animal_create, name="animals_create"),
+    path('animals_m', views.show_animals, name="animals_m"),
+    path('animals/<int:pk>', views.AnimalDetailView.as_view(), name="detail_view")
 ]
